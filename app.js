fd769335081e92bbc3da6a80795d9e4e0e794704
Seif <sofa5060@gmail.com>
2020-01-27 14:31:57 +0200

    working oon animation

---
+++ app.js
@@ -0,0 +1,7 @@
+const move = () => {
+  document.getElementsByClassName("secondery-video")[0].classList.add("move");
+};
+
+const move2 = () => {
+  document.getElementsByClassName("secondery-video")[0].classList.remove("move");
+};
